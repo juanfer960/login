bf5c2dba242452ba5b876398a38c11ad944e26cf
Juan Fernando Zuluaga Ramirez <juanfernandozuluagaramirez@MacBook-Pro-de-Juan.local>
2019-08-15 20:47:52 -0500

    primer commit

---
+++ envProyecto/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/juanfernandozuluagaramirez/Documents/Cloud/Proyecto_0/gestionDeEventos/envProyecto/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
